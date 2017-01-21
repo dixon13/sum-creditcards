4ae7574a6a3f9659d946f125619838c23c1fa0d2
dixon13 <dixonbegay@gmail.com>
2017-01-21 11:46:52 -0700

    Adding App.js from other file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var cards = ['4916-2600-1804-0530', '4779-252888-3972', '4252-278893-7978', '4556-4242-9283-2260'];
+var sumCards = [];
+// var theLog = [];
+
+function cardNumbersGreatestSum(array) {
+  var temp_array = [];
+  array.forEach(function(element) {
+    // element = element.split('');
+    var temp = element.replace(/-/g, ''); // "1234567890" Removing hyphens
+    var temp1 = temp.split('');
+    temp_array.push(temp1);
+  });
+  
+  temp_array.forEach(function(item) {
+    item.forEach(function(ele) {
+      var hash = [];
+      var int = Number(ele);
+      var index = item.indexOf(ele);
+      item[index] = int;
+      if (index === 0) {
+        sumCards.push(["hash", hash]);
+      }
+      console.log("Adding: ", item[index] + item[0]);
+      hash.push(item[index] + item[0]);
+    });
+    // console.log("-----");
+    // for (var count = 0; count < (item.length + 1); count++) {
+    //   console.log(item[count]);
+    // }
+  });
+  // for (var item in array) {
+  //   item.replace('-', '');
+  //   item.split("");
+  //   theLog.push(item);
+  // }
+  return sumCards
+}
+var returned = cardNumbersGreatestSum(cards);
+console.log("Returned: ", returned);
+
+// console.log("theLog: ", theLog);
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +55,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{returned}</p>
       </div>
     );
   }
