ed522867130e691d2876966bf395000d3712a0b0
Dixon Begay <dixonbegay@gmail.com>
2017-01-22 21:15:31 -0700

    Now returns sum of credit cards

--- src/App.js
+++ src/App.js
@@ -3,46 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 var cards = ['4916-2600-1804-0530', '4779-252888-3972', '4252-278893-7978', '4556-4242-9283-2260'];
-var sumCards = [];
-// var theLog = [];
 
 function cardNumbersGreatestSum(array) {
+  var sumCards = [];
+  console.log("Input: ", array);
   var temp_array = [];
-  array.forEach(function(element) {
-    // element = element.split('');
-    var temp = element.replace(/-/g, ''); // "1234567890" Removing hyphens
-    var temp1 = temp.split('');
-    temp_array.push(temp1);
-  });
-  
-  temp_array.forEach(function(item) {
-    item.forEach(function(ele) {
-      var hash = [];
-      var int = Number(ele);
-      var index = item.indexOf(ele);
-      item[index] = int;
-      if (index === 0) {
-        sumCards.push(["hash", hash]);
+
+  for (var i = 0; i < array.length; i++) {
+    console.log("Removing hyphens and zeros...");
+    array[i] = array[i].replace(/[^1-9]/g, ''); // "123456789" Removing hyphens and zeros
+    array[i] = array[i].split('');
+    temp_array.push(array[i]);
+    console.log(array[i]);
+    console.log("temp_array: ", temp_array);
+  }
+
+  for (var x = 0; x < temp_array.length; x++) {
+    console.log("Looping through temp_array - new array with no hyphens");
+    console.log(temp_array[x]);
+    for (var y = 0; y < temp_array[x].length; y++) {
+      if (y === 0) {
+        // console.log("LOG: ", temp_array[x][y]);
+        console.log("Adding to sumCards: ", temp_array[x][y]);
+        sumCards.push(Number(temp_array[x][y]));
+      } else {
+        var sum = sumCards[sumCards.length - 1] + Number(temp_array[x][y]);
+        sumCards[sumCards.length - 1] = sum;
       }
-      console.log("Adding: ", item[index] + item[0]);
-      hash.push(item[index] + item[0]);
-    });
-    // console.log("-----");
-    // for (var count = 0; count < (item.length + 1); count++) {
-    //   console.log(item[count]);
-    // }
-  });
-  // for (var item in array) {
-  //   item.replace('-', '');
-  //   item.split("");
-  //   theLog.push(item);
-  // }
+    }
+  }
   return sumCards
 }
 var returned = cardNumbersGreatestSum(cards);
 console.log("Returned: ", returned);
-
-// console.log("theLog: ", theLog);
 
 class App extends Component {
   render() {
